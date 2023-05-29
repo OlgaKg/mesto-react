16cafde64645378654c0e3615725d72cc866af51
OlgaKG <flory_oli@mail.ru>
2023-05-30 00:36:55 +0300

    10PR/1review

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,15 @@
+import React, {useState, useEffect} from 'react';
+
 function Footer() {
+    const [date , setDate] = useState();
+    const getYear = () =>  setDate(new Date().getFullYear())
+    useEffect(() => {
+        getYear();
+    }, [])
+
     return (
         <footer className="footer">
-            <p className="footer__copyright">&copy; 2022 Mesto Russia</p>
+            <p className="footer__copyright">&copy; {date} Mesto Russia</p>
         </footer>
     );
 }
